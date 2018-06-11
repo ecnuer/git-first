3dc5560b1d8e884731e922fecae2fe7d7243dbdc
liuweiwei <liuww@gegejia.com>
2018-06-11 14:31:16 +0800

    commit branch test2

--- src/com/wwl/First.java
+++ src/com/wwl/First.java
@@ -5,6 +5,7 @@ public class First {
 		System.out.println(System.getProperty("user.home"));
 		System.out.println("hhhh");
 		System.out.println("this is in a branch");
+		System.out.println("now in test2 branch");
 	}
 
 }
