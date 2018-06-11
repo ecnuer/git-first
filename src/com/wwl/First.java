4cf634d2f1cc241c175f098c0fc6720b519ebf06
liuweiwei <liuww@gegejia.com>
2018-06-11 13:18:49 +0800

    just test branch commit

--- src/com/wwl/First.java
+++ src/com/wwl/First.java
@@ -4,6 +4,7 @@ public class First {
 	public static void main(String[] args){
 		System.out.println(System.getProperty("user.home"));
 		System.out.println("hhhh");
+		System.out.println("this is in a branch");
 	}
 
 }
